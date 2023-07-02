4391db2fa949351b5aa3bbe217334cfc43747fb1
sinjeeli <sinjeeli@gmail.com>
2023-07-02 13:13:48 -0400

    working state

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const data = require('./data.json');
+const path = require('path');
+//
+console.log('hello');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+//
+app.get('/', (req, res) => {
+    res.render('index', { projects: data.projects });
+  });
+  
+  app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  
+  app.get('/project/:id', (req, res) => {
+    const projectId = req.params.id;
+    const project = data.projects[projectId];
+    res.render('project', { project });
+  });
+
+  //
+  const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//
